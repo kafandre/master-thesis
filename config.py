5a45cf82109b93fcdbc86860bd2fa629e25658e7
kafandre <andre.kafanke@coac.de>
2025-03-20 16:00:31 +0100

    Add train_size calculation to config

--- config.py
+++ config.py
@@ -4,13 +4,14 @@ class config:
     SEED = 111              # Global random seed for reproducibility
     data_seed = 423         # Seed specifically for data generation
     train_split = 0.8       # Proportion of data to use for training
+    train_size = int(data_amount * train_split)
 
     # Model parameters
     n_estimators = 1000     # Number of estimators for the CWB model
     learning_rate = 0.1     # Learning rate for the CWB model
     eval_freq = 1           # Evaluation frequency (iterations)
-    batch_size = 40         # Batch size for training
+    batch_size = train_size       # Batch size for training
     batch_mode = "all"
     
     # Flooding parameters
-    flood_level = 33        # Flooding level for the flooding loss
+    flood_level = 0        # Flooding level for the flooding loss
