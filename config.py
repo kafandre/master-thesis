64af70235eb50d7d02b33778ca536b3d46f41937
kafandre <andre.kafanke@coac.de>
2026-02-09 16:43:10 +0100

    Update config: hyperparams, scenarios

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 BASE_LEARNERS_LIST = ["linear", "polynomial", "tree", "bspline"]
 
 class config:
-    SEED = 500
-    n_seeds = 30
+    SEED = 600
+    n_seeds = 5
     
     # --- Dataset Configuration ---
     # Options: "synthetic" or "real"
@@ -15,12 +15,12 @@ class config:
     base_learners = BASE_LEARNERS_LIST
 
     # Static Hyperparameters
-    poly_degree = 2
+    poly_degree = 3
     tree_depth = 1
     
     # B-Spline Configuration
-    spline_degree = 1
-    n_knots = 5  # Number of internal knots
+    spline_degree = 3
+    n_knots = 25  # Number of internal knots
     
     # Tree Configuration
     n_bins = 32
@@ -35,8 +35,8 @@ class config:
     # Experiment Config
     n_estimators = 1000
     learning_rate = 0.05
-    train_split = 0.7
-    val_split = 0.15
+    train_split = 0.4
+    val_split = 0.10
 
     # --- Evaluation Configuration ---
     # Scenarios for drift evaluation (Type, Magnitude)
@@ -55,32 +55,36 @@ class config:
     # Defines the 7 stress-test environments
     SCENARIOS = {
         "Baseline": {
-            "n_samples": 500, "dim": 50, "noise_std": 5.0,
-            "signal_type": "linear_interaction", "feature_dist": "normal", "noise_dist": "normal"
+            "n_samples": 1000, "dim": 100,  "signal_scale": 1.0, "noise_std": 5.0,
+            "signal_type": "baseline_composite", "feature_dist": "normal", "noise_dist": "normal"
         },
         "HighDims": {
-            "n_samples": 250, "dim": 250, "noise_std": 5.0,
-            "signal_type": "linear_interaction", "feature_dist": "normal", "noise_dist": "normal"
+            "n_samples": 500, "dim": 1000,  "signal_scale": 1.0, "noise_std": 5.0,
+            "signal_type": "baseline_composite", "feature_dist": "normal", "noise_dist": "normal"
         },
-        "Friedman": {
-            "n_samples": 500, "dim": 50, "noise_std": 1.0, 
-            "signal_type": "friedman", "feature_dist": "normal", "noise_dist": "normal"
+        "High_Noise": {
+            "n_samples": 1000, "dim": 100, "signal_scale": 1.0, "noise_std": 10.0,
+            "signal_type": "baseline_composite", "feature_dist": "normal", "noise_dist": "normal"
+        },    
+        "Multicollinearity": {
+            "n_samples": 1000, "dim": 100,  "signal_scale": 1.0, "noise_std": 5.0,
+            "signal_type": "baseline_composite", "feature_dist": "correlated", "noise_dist": "normal"
+        },
+        "Linear": {
+            "n_samples": 1000, "dim": 100,  "signal_scale": 1.0, "noise_std": 5.0,
+            "signal_type": "simple_additive", "feature_dist": "normal", "noise_dist": "normal"
+        },                
+        "Smooth": {
+            "n_samples": 1000, "dim": 100,  "signal_scale": 1.0, "noise_std": 5.0, 
+            "signal_type": "smooth_qubic", "feature_dist": "normal", "noise_dist": "normal"
+        },
+        "High_Freq": {
+            "n_samples": 1000, "dim": 100,  "signal_scale": 1.0, "noise_std": 5.0,
+            "signal_type": "high_freq", "feature_dist": "normal", "noise_dist": "normal"
         },
         "Step": {
-            "n_samples": 500, "dim": 50, "noise_std": 5.0,
+            "n_samples": 1000, "dim": 100, "signal_scale": 1.0, "noise_std": 2.0,
             "signal_type": "step", "feature_dist": "normal", "noise_dist": "normal"
-        },
-        "Multicollinearity": {
-            "n_samples": 500, "dim": 50, "noise_std": 5.0,
-            "signal_type": "linear_interaction", "feature_dist": "correlated", "noise_dist": "normal"
-        },
-        "Outlier_Features": {
-            "n_samples": 500, "dim": 50, "noise_std": 5.0,
-            "signal_type": "linear_interaction", "feature_dist": "student_t", "noise_dist": "normal"
-        },
-        "Outlier_Target": {
-            "n_samples": 500, "dim": 50, "noise_std": 5.0,
-            "signal_type": "linear_interaction", "feature_dist": "normal", "noise_dist": "student_t"
         }
     }
 
@@ -92,11 +96,11 @@ class config:
         for scenario in SCENARIOS
     }
 
+    TUNED_LRS["Step"]["tree"] = 0.1
     # TUNED_LRS["Baseline"]["tree"] = 0.1
-    # TUNED_LRS["HighDims"]["linear"] = 0.01
-
-    # --- Demo / Single Run Configuration (for train.py __main__) ---
-    demo_seed = 100
-    demo_scenario = "Baseline" # Used to pick from SCENARIOS in demo
-    demo_flood_multiplier = 1.0
-    demo_use_flooding = False
+    # TUNED_LRS["Baseline"]["bspline"] = 0.1
+    # TUNED_LRS["HighDims"]["linear"] = 0.1
+    # TUNED_LRS["HighDims"]["bspline"] = 0.08
+    # TUNED_LRS["HighDims"]["tree"] = 0.1
+    # TUNED_LRS["HighDims"]["polynomial"] = 0.1
+    
